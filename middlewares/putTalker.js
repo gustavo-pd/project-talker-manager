f1ac32c263acdcfb8de1f7f549257cd80a7d0fed
GustavoPD <gudalmoro@gmail.com>
2022-04-04 17:18:28 -0300

    ai minha vuaida

--- middlewares/putTalker.js
+++ middlewares/putTalker.js
@@ -1,23 +1,20 @@
 const fs = require('fs');
 const readJsonFile = require('../helpers/readJsonFile');
 
-const file = 'talker.json';
 const HTTP_OK_STATUS = 200;
 
 function putTalker(req, res) {
-  let { id } = req.params;
   const { name, age, talk } = req.body;
-  const talkers = readJsonFile(file);
+  let { id } = req.params;
 
-  const findId = talkers.findIndex((i) => i.id === parseInt(id, 10));
+  const talkers = readJsonFile('talker.json');
+  const findId = talkers.findIndex((i) => i.id === Number(id));
   talkers[findId] = { ...talkers[findId], name, age, talk };
 
-  const talkersList = JSON.stringify(talkers);
+  fs.writeFileSync('talker.json', JSON.stringify(talkers));
+  id = Number(id);
 
-  fs.writeFileSync(file, talkersList);
-  id = parseInt(id, 10);
-
-  return res.status(HTTP_OK_STATUS).json({ name, age, id, talk });
+  return res.status(HTTP_OK_STATUS).json({ id, name, age, talk });
 }
 
 module.exports = putTalker;

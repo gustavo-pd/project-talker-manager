0952b0cfac226e5afaec88affdd1133d172fd3f5
GustavoPD <gudalmoro@gmail.com>
2022-04-04 17:02:50 -0300

    teste

--- middlewares/putTalker.js
+++ middlewares/putTalker.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 const readJsonFile = require('../helpers/readJsonFile');
 
-const file = 'talker.json';
 const HTTP_OK_STATUS = 200;
 
 function putTalker(req, res) {
   let { id } = req.params;
   const { name, age, talk } = req.body;
-  const talkers = readJsonFile(file);
+  const talkers = readJsonFile('talker.json');
 
   const findId = talkers.find((i) => i.id === Number(id));
   talkers[findId] = { ...talkers[findId], name, age, talk };

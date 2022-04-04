f1ced99990b5b22808eaf4baf504f99b7779a2e0
GustavoPD <gudalmoro@gmail.com>
2022-04-04 19:03:40 -0300

    Ai minha vuaida pt4

--- middlewares/putTalker.js
+++ middlewares/putTalker.js
@@ -8,10 +8,10 @@ function putTalker(req, res) {
   const { name, age, talk } = req.body;
   const { id } = req.params;
   const talkers = readJsonFile(file);
-  const findId = talkers.findIndex((i) => i.id === Number(id));
+  const findId = talkers.findIndex((i) => i.id === parseInt(id, 10));
   talkers[findId] = { ...talkers[findId], name, age, talk };
-
-  fs.writeFileSync(file, JSON.stringify(talkers));
+  const talkersList = JSON.stringify(talkers, null, 2);
+  fs.writeFileSync(file, talkersList);
 
   return res.status(HTTP_OK_STATUS).json(talkers[findId]);
 }

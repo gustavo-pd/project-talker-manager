8ef6e7e9ac36364755049c107c374518722547d7
GustavoPD <gudalmoro@gmail.com>
2022-04-04 17:21:55 -0300

    passo

--- middlewares/putTalker.js
+++ middlewares/putTalker.js
@@ -2,16 +2,17 @@ const fs = require('fs');
 const readJsonFile = require('../helpers/readJsonFile');
 
 const HTTP_OK_STATUS = 200;
+const file = 'talker.json';
 
 function putTalker(req, res) {
   const { name, age, talk } = req.body;
   let { id } = req.params;
 
-  const talkers = readJsonFile('talker.json');
+  const talkers = readJsonFile(file);
   const findId = talkers.findIndex((i) => i.id === Number(id));
   talkers[findId] = { ...talkers[findId], name, age, talk };
 
-  fs.writeFileSync('talker.json', JSON.stringify(talkers));
+  fs.writeFileSync(file, JSON.stringify(talkers));
   id = Number(id);
 
   return res.status(HTTP_OK_STATUS).json({ name, age, id, talk });
